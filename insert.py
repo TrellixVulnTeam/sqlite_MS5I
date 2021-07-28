b960e16bc4e6aa32ffde9955f1d2b5db03f57a3d
takahashi_daiwa <onogami27@gmail.com>
2021-07-28 13:46:48 +0900

    更新

--- insert.py
+++ insert.py
@@ -1,12 +1,14 @@
 import datetime
 import sqlite3
 import os
-now_time = str(datetime.datetime.now())
-file_name = os.path.basename(r"C:\Users\onoga\OneDrive\Desktop\検証用フォルダ\229-772-82711-111111-1修理)高橋.xlsx")
+
 
 conn = sqlite3.connect("main_data.db")
 c = conn.cursor()
 
 #値をインサートする
-c.execute("insert into main values(?,?)",(now_time,file_name))
-conn.commit()
+def insert_act(file):
+    now_time = str(datetime.datetime.now())
+    file_name = os.path.basename(file)
+    c.execute("insert into main values(?,?)",(now_time,file_name))
+    conn.commit()
---
+++ test_sginsert.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+import sqlite3
+import datetime
+import os
+
+conn = sqlite3.connect("main_data.db")
+c = conn.cursor()
+
+#column名を取得する関数
+def colum_name():
+   
+    c.execute("select * from main")
+    out = []
+    for i in c.description:
+        out.append(i[0])
+        
+    return out
+
+
+
+
+def select_act():
+    
+    c.execute("select * from main")
+    out = c.fetchall()
+
+    return out
+
+#値をインサートする
+def insert_act(file):
+    now_time = str(datetime.datetime.now())
+    file_name = os.path.basename(file)
+    c.execute("insert into main values(?,?)",(now_time,file_name))
+    conn.commit()
+
+for i in select_act():
+    print(i)
+
+print(colum_name())
