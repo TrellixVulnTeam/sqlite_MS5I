f2c0e89b2b21bf1318f36553116acf40408eb666
takahashi_daiwa <onogami27@gmail.com>
2021-07-28 14:50:49 +0900

    更新

--- sisaku_1.py
+++ sisaku_1.py
@@ -4,7 +4,7 @@ import test_sginsert
 
 lay = [
     [sg.InputText(key="in"),sg.FileBrowse()],
-    [sg.Button("データ取り込み",key="insert"),sg.Button("更新",key="update")],
+    [sg.Button("データ取り込み",key="insert"),sg.Button("更新",key="update"),sg.Button("得る",key="get")],
     [sg.Table(values=test_sginsert.select_act(),headings=test_sginsert.colum_name(),auto_size_columns=False,
     def_col_width=30,key="table")]
 ]
@@ -18,7 +18,16 @@ while True:
         break
     
     if event == "insert":
-        test_sginsert.insert_act(os.path.basename(os.path.splitext(values["in"])[0]))
-        window["table"].update(values=test_sginsert.select_act())
+        if values["in"] == "":
+            sg.popup("データを選択してください")
+            continue
+        else:
+
+            test_sginsert.insert_act(os.path.basename(os.path.splitext(values["in"])[0]))
+            window["table"].update(values=test_sginsert.select_act())
     if event == "update":
         window["table"].update(values=test_sginsert.select_act())
+
+    if event == "get":
+        x = window["table"].get()
+        print(x)
