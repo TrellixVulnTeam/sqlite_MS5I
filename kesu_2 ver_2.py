0a68e6acccec40145ce78a800b6ecef8fb91a71d
onogami27 <onogami27@gmail.com>
2021-09-06 07:15:10 +0900

    9/6am

--- kesu_2 ver_2.py
+++ kesu_2 ver_2.py
@@ -1,7 +1,11 @@
 
+from tkinter import Event
+from tkinter.constants import FALSE
 import PySimpleGUI as sg
 from plyer import notification
 import time
+from psgtray import SystemTray
+import re
 
 #画面がぼやけるのを回避するコード
 import ctypes
@@ -13,6 +17,7 @@ except:
 
 settings = sg.UserSettings(autosave=True)
 settings.load()
+
 
 
 demo_layout = [[sg.Text("テスト"),sg.InputText()]]
@@ -31,9 +36,10 @@ def coll():
 lay = [
     [sg.Menu([["メニュー",["上書き保存"]],
              ["追加",["タスクを追加","aaa","seting"]],])],
-    [sg.Column(layout=
+             
+    [sg.pin(sg.Column(layout=
 
-    [],key="col")],
+    [],key="col",vertical_scroll_only=True,scrollable=True,size=(580,200)))],
 
 
 ]
@@ -44,7 +50,11 @@ lay = [
 
 
 
-window = sg.Window("test",layout=lay,enable_close_attempted_event=True,finalize=True,use_custom_titlebar=True,)
+window = sg.Window("タスク管理",layout=lay,enable_close_attempted_event=True,finalize=True,use_custom_titlebar=True,keep_on_top=True,grab_anywhere=True)
+
+menu = ['', ['メニュー',["上書き保存"],"追加",["タスクを追加","aaa","seting"]]]
+tray = SystemTray(menu=menu,tooltip="タスク管理",single_click_events=False,window=window)
+
 
 
 count = 1
@@ -57,6 +67,10 @@ while True :
     if event == sg.WIN_X_EVENT:
         break
     
+    #システムトレイのメニューコマンドを使えるようにする記述
+    if event == tray.key:
+            event = values[event] 
+
     if event == None:
         print(event)
         break
@@ -66,11 +80,29 @@ while True :
         print(settings)
 
     if event == "上書き保存":
-        pass
+      
+        coll()
     if event == "タスクを追加":
-        window.extend_layout(window["col"],[[sg.Text(f"No.{count}"),sg.InputText(key=f"input_{count}"),sg.Button("完了",key=f"ok_{count}")]])
+        settings[f"input{count}"] = ""
+        window.extend_layout(window["col"],[([sg.Text(f"No.{count}",key=f"No._{count}"),sg.InputText(default_text=settings[f"input_{count}"],key=f"input_{count}"),sg.Button("完了",key=f"ok_{count}")])])
+        #countの値をユーザー設定に追加
+        settings["count"] = count
         count += 1
+        #下記の一文を追加しないと更新されずスクロールバーが機能しない
+        window["col"].contents_changed()
+        
         
     if event == "aaa":
-        pass
-   
+       print(values)
+
+    if bool(re.search("ok_*",event)) == True:
+        no = re.split("ok_",event)[1]
+        [window[f"{i}{no}"].hide_row() for i in ["input_","ok_","No._"]]
+        settings[f"input_{no}"] = False
+        settings[f"ok_{no}"] = False
+        settings[f"No._{no}"] = False
+        window["col"].contents_changed()
+        
+
+    #print(event)
+    #print(bool(re.search("ok_*",event)))
--- kesususususu.py
+++ kesususususu.py
@@ -1,5 +1,32 @@
-import PySimpleGUIQt as sg
+import PySimpleGUI as sg
 
-lay = [[sg.InputText(),sg.Button("完了")]]
 
-window = sg.Window("test",layout=lay).read()
+column = sg.Column(
+    [[sg.pin(sg.Frame('', [[
+        sg.T(str(i)), sg.OK(), sg.Cancel()]],
+        key=str(i), visible=False))] for i in range(10)],
+    scrollable=True, vertical_scroll_only=True, size=(300, 330))
+layout = [[column]] + [
+    [sg.Button('Add row', key='-B1-')] + [sg.Button('Del row', key='-B2-')]
+]
+
+
+window = sg.Window('Window Title', layout)
+i = 0
+while True:  # Event Loop
+    event, values = window.read()
+    if event in (None, 'Exit'):
+        break
+    if event == '-B1-':
+        if i > 9:
+            i = 9
+        window[str(i)](visible=True)
+        i += 1
+        column.contents_changed()
+    if event == '-B2-':
+        i -= 1
+        if i < 0:
+            i = 0
+        window[str(i)](visible=False)
+        column.contents_changed()
+window.close()
