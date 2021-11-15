1156e63ddbf0fd37a477ad02f1ef8a0477621831
onogami27 <onogami27@gmail.com>
2021-11-16 07:12:48 +0900

    11/16am

--- onogam/rank.py
+++ onogam/rank.py
@@ -1,16 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pandas.core.frame import DataFrame
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 
 data = pd.read_excel("dataset.xlsx",header=0)
 
+y = data.loc[:,["評価"]]
+yy = data["評価"]
+x = data.loc[:,["データ"]]
+xx = data["データ"]
 
-y = data["評価"]
-x = data["データ"]
-print(y[::])
+clf = svm.SVC(kernel="linear")
 
-#clf = svm.SVC(kernel="linear")
+clf.fit(x,y)
 
-#clf.fit(x,y)
+test_x =data.loc[:,["結果データ"]]
+test_x = test_x.dropna(how="any",axis=1)
+test_y =data.loc[:,["結果"]]
+
+test_y = test_y.dropna(how="any",axis=1)
+
+print(clf.predict(test_x))
