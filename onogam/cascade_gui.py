1120d616c6a8e3a65670eb7c47f551d2dd34eac0
onogami27 <onogami27@gmail.com>
2021-12-06 07:07:00 +0900

    12/6am

--- onogam/cascade_gui.py
+++ onogam/cascade_gui.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 import os
+import PySimpleGUI as sg
 
 count = 0
+sg.set_options(use_ttk_buttons=True)
+sg.theme("DarkGrey2")
 
 #カレントディレクトリのパスを取得する
 def get_path(path):
@@ -17,6 +20,13 @@ def get_path(path):
         file_list.append(x)
     
     return file_list
+
+#画面がぼやけるのを回避するコード
+import ctypes
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(True)
+except:
+    pass
 
 
 num = 1
@@ -83,35 +93,59 @@ def main(img_path):
                 #cv2.line(img2, pt_1[0], (x, pt_1[0][1]), (255, 0, 0))
                 #cv2.line(img2, pt_1[0], (pt_1[0][0], y), (255, 0, 0))
                 cv2.imshow("", img2) 
-        
-               
                 
     cv2.imshow("", img)
 
     #マウスイベント（コールバック関数）を定義
     cv2.setMouseCallback("", event_mouse,)
-
     cv2.waitKey()
     cv2.destroyAllWindows()
+    
     return {"total" :total , "num" :num[0]}
 
-file_path = get_path(r"C:\Users\onoga\desktop\test_pic")
-lec = ""
-#メイン
-for i in file_path:
-    mes = main(i)
-    total = mes["total"]
-    num = mes["num"]
-    for ff in total:
-        lec = lec + f" {ff}"
-    final = f"{num}",lec
-    #f = open("poslist.txt", "w")
+#レイアウト
+lay = [
+    [sg.Text("画像ファイルが格納してあるフォルダを選択してください")],
+    [sg.InputText(key="input"), sg.FolderBrowse(button_text="選択")],
+    [sg.Text("出力先のフォルダを選択してください")],
+    [sg.InputText(key="output"), sg.FolderBrowse(button_text="選択")],
+    [sg.Text("出力ファイル名"),sg.InputText(default_text="poslist.txt",size=(20,10),key=("out_name"))],
+    [sg.Button("開始",key="bt_start")]
     
-    #f.write(f"{i},{final[0]},{final[1]}\n")
-    #f.close()
-    file_name = os.path.join(os.getcwd(),"poslist.txt")
-    f = open(file_name, "a")
-    f.write(f"{i} {final[0]}{final[1]}\n")
- 
-    lec = ""
+]
+
+
+window = sg.Window("画像　アノテーション　ツール", lay)
+
+while True:
+    event,value = window.read()
     
+    if event == None:
+        break
+    
+    if event == "bt_start":
+        if value["input"] == "":
+            sg.popup("選択されていない項目があります")
+            continue
+        if value["output"] == "":
+            sg.popup("選択されていない項目があります")
+            continue
+        if value["out_name"] == "":
+            sg.popup("選択されていない項目があります")
+            continue
+        file_path = get_path(value["input"])
+        lec = ""
+        #メイン
+        for i in file_path:
+            mes = main(i)
+            total = mes["total"]
+            num = mes["num"]
+            for ff in total:
+                lec = lec + f" {ff}"
+            final = f"{num}",lec
+            #テキストファイルに書き込み
+            file_name = os.path.join(value["output"],value["out_name"])
+            f = open(file_name, "a")
+            f.write(f"{i} {final[0]}{final[1]}\n")
+            #lecをクリア
+            lec = ""
