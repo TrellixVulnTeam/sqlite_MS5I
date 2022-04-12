9ad2a801fe143a2e9a8fc4570a19282ab46c196a
髙橋 克弥 <60837@KPSYSTEM.kojima-tns.co.jp>
2022-04-12 16:59:19 +0900

    4/12da

--- ZIP/kokonara.py
+++ ZIP/kokonara.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg
 
-sg.theme("LightBlue3")
+sg.theme(new_theme="LightTeal")
 
-sg.set_options(dpi_awareness=True,)
+sg.set_options(dpi_awareness=True,use_ttk_buttons="DEFAULT_TTK_THEME",)
 
 listut = ["バルセロナ","レアルマドリード","チェルシー","バイエルンミュンヘン","リヴァプール","マンチェスターシティ"]
 
@@ -10,9 +10,12 @@ listut = ["バルセロナ","レアルマドリード","チェルシー","バイ
 
 lay = [
     [sg.Input(),sg.Button("選択")],
-    [sg.Listbox(listut,size=(30,6))],
-    [sg.Radio("リンゴ",group_id="A"),sg.Radio("ゴリラ",group_id="A"),sg.Radio("ラッパ",group_id="A")],
+    #[sg.Listbox(listut,size=(30,6))],
+    #[sg.Radio("リンゴ",group_id="A"),sg.Radio("ゴリラ",group_id="A"),sg.Radio("ラッパ",group_id="A")],
     [sg.Multiline(size=(50,10))],
+    [sg.Button("change",key = "change")],
+    [sg.Checkbox("A"),sg.Checkbox("B")],
+    [sg.Menu([["ファイル",["aaa"]],["編集"],["表示"],["オプション"]],tearoff=False,)],
     
     
 ]
@@ -24,3 +27,6 @@ while True:
     
     if event == None:
         break
+    
+    if event == "change":
+        pass
