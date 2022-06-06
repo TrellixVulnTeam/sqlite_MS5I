a451c40b0a767a6bb1ee68bd58e434577c9d8586
onogami27 <onogami27@gmail.com>
2022-06-07 07:22:23 +0900

    6/7am

--- onogam/tes__1.py
+++ onogam/tes__1.py
@@ -1,4 +1,16 @@
 import tempfile
+import cv2
+
+#img = cv2.imread(r"C:\Users\onoga\Desktop\MyDocker\Git\origin\test\pos\2.png")
+#cv2.imshow("",img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 with tempfile.NamedTemporaryFile(delete=False) as tf:
-    print(tf.name)
+    
+    #print(tf.name)
+    #with open(tf.name) as f:
+    #    f.write(img)
+    #    print(f.read())
+        
+tf.close()
