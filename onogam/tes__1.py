8cf3a9deb4ae152c4d7207195528be97778fc6a0
onogami27 <onogami27@gmail.com>
2022-06-08 06:57:46 +0900

    6/8am

--- onogam/tes__1.py
+++ onogam/tes__1.py
@@ -1,16 +1,17 @@
 import tempfile
 import cv2
+import os 
 
-#img = cv2.imread(r"C:\Users\onoga\Desktop\MyDocker\Git\origin\test\pos\2.png")
-#cv2.imshow("",img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+img = cv2.imread(r"C:\Users\onoga\Desktop\MyDocker\Git\origin\test\pos\2.png")
 
-with tempfile.NamedTemporaryFile(delete=False) as tf:
-    
-    #print(tf.name)
-    #with open(tf.name) as f:
-    #    f.write(img)
-    #    print(f.read())
-        
+
+with tempfile.NamedTemporaryFile(delete=True) as tf:
+    cv2.imwrite(f"{tf.name}.jpg" , img)#一時ファイルに画像形式で書き込む
+ 
+    tf.seek(0)#先頭に戻る
+    os.chdir(os.path.split(tf.name)[0])#一時ファイルの位置にカレントディレクトリを移動する
+    iii = cv2.imread(f"{tf.name}.jpg")#←これが重要　CV2で読み込む形式に変換
+    cv2.imshow("" , iii)
+    cv2.waitKey(0)
 tf.close()
+
