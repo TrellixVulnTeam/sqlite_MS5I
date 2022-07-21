6e22dd4023ba433ce640c2a9f9e50804277f7da8
onogami27 <onogami27@gmail.com>
2022-07-22 07:15:20 +0900

    7/22am

--- onogam/iiiiiooo.py
+++ onogam/iiiiiooo.py
@@ -4,8 +4,10 @@ import PySimpleGUI as sg
 from PIL import Image
 
 img = cv2.imread("onono.jpg")
-cv2.putText(img=img,text="hellow",org=(100,100),fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=1.5,color=(0,250,0))
+height, width, channels = img.shape[:3]
+print(height,width)
+cv2.putText(img=img,text="1/300",org=(width-180,50),fontFace=cv2.FONT_ITALIC,
+            fontScale=1.5,color=(250,0,0),thickness=2)
 
 cv2.imshow("",img)
 cv2.waitKey(0)
