24cc2f595a594a2089a23d3ca2410675021db6d5
髙橋 克弥 <60837@KPSYSTEM.kojima-tns.co.jp>
2022-09-09 17:20:42 +0900

    9/9da

---
+++ YoLo/PLAY_DETECT.py
@@ -0,0 +1,44 @@
+from tkinter.tix import ButtonBox
+import PySimpleGUI as sg
+import os
+import subprocess
+
+sg.theme("SystemDefault")
+
+sg.set_options(font=("meiryo",8),use_ttk_buttons=True,dpi_awareness=True)
+
+CD = os.getcwd()
+
+lay_img = sg.Tab("画像",[
+    [sg.Text("カレントディレクトリ"),sg.InputText(default_text=CD,key="img_cd",size=(30,1),readonly=True),sg.FolderBrowse("変更"),sg.Button("保存",key="img_save",button_color="#008000")],
+    [sg.Text("検出する画像フォルダを選択"),sg.InputText(key="img_in",size=(30,1)),sg.FolderBrowse("選択")],
+    [sg.Text("学習データを選択(.ptファイル)"),sg.InputText(key="img_pt",size=(30,1)),sg.FileBrowse("選択")],
+    [sg.Text("判定の閾値 [--conf]"),sg.InputText(default_text=0.4, size=(10,1),key="img_conf")],
+    [sg.Text("保存するディレクトリを指定"),sg.InputText(default_text="runs/detect",key="img_project",size=(30,1)),sg.FolderBrowse("選択")],
+    [sg.Text("学習データを保存するディレクトリを指定"),sg.InputText(default_text="exp",key="img_dir_name",size=(20,1))],
+
+    [sg.Button("START",key="START_IMG")],
+    
+])
+
+lay_cap = sg.Tab("動画",[
+    [sg.Text("")]
+])
+
+layout = [[sg.TabGroup([[lay_img,lay_cap]])]]
+
+window = sg.Window("",layout=layout)
+while True:
+    event, value = window.read()
+    
+    if event == None:
+        break
+
+    if event == "START_IMG":
+        subprocess.run("python detect.py --source {0} --weights {1} --conf {2} --project {3} --name {4}".format(value["img_in"],value["img_pt"],value["img_conf"],value["img_project"],value["img_dir_name"]),shell=True)
+        
+    if event == "img_save":
+        CD = value["img_cd"]
+        os.chdir(CD)
+        window["img_cd"].update(CD)
+        
