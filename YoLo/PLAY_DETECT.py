f139dd09a22fec8f175d520eb15892cc795b8020
onogami27 <onogami27@gmail.com>
2022-09-13 07:19:13 +0900

    9/13am

--- YoLo/PLAY_DETECT.py
+++ YoLo/PLAY_DETECT.py
@@ -22,7 +22,10 @@ lay_img = sg.Tab("画像",[
 ])
 
 lay_cap = sg.Tab("動画",[
-    [sg.Text("")]
+    [sg.Text("学習データを選択(.ptファイル)"),sg.InputText(key="cap_pt",size=(30,1)),sg.FileBrowse("選択")],
+    [sg.Text("判定の閾値 [--conf]"),sg.InputText(default_text=0.4, size=(10,1),key="cap_conf")],
+    [sg.Text("画像のサイズ[--img]"),sg.InputText(default_text=640, size=(10,1),key="cap_img")],
+    [sg.Button("START",key="cap_IMG")],
 ])
 
 layout = [[sg.TabGroup([[lay_img,lay_cap]])]]
@@ -33,12 +36,17 @@ while True:
     
     if event == None:
         break
-
+    #画像処理開始
     if event == "START_IMG":
         subprocess.run("python detect.py --source {0} --weights {1} --conf {2} --project {3} --name {4}".format(value["img_in"],value["img_pt"],value["img_conf"],value["img_project"],value["img_dir_name"]),shell=True)
-        
+    
+    #カレントディレクトリを保存    
     if event == "img_save":
         CD = value["img_cd"]
         os.chdir(CD)
         window["img_cd"].update(CD)
+        
+    #動画処理開始
+    if event == "cap_IMG":
+        subprocess.run("python detect.py --source 0 --weights {0} --conf {1} --img {2}".format(value["cap_pt"],value["cap_conf"],value["cap_img"]),shell=True)
         
