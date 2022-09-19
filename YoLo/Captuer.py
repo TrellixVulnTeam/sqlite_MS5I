12267f4e9bc8aaefee5e12e8fce6131df0a668ad
髙橋 克弥 <60837@KPSYSTEM.kojima-tns.co.jp>
2022-09-19 17:26:58 +0900

    9/19da

--- YoLo/Captuer.py
+++ YoLo/Captuer.py
@@ -6,22 +6,29 @@ sg.theme("SystemDefault")
 cd = os.getcwd()
 sg.set_options(dpi_awareness=True,use_ttk_buttons=True,font="NSimSun")
 
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+default_FRAME_HEIGHT = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+default_FRAME_WIDTH = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+
 lay = [
     [sg.Text("保存するフォルダを指定"),sg.InputText(key="in",default_text=cd),sg.FolderBrowse("選択")],
-    [sg.Text("ファイル名を指定"),sg.InputText(key="file_name",default_text="img_")],
+    [sg.Text("保存するファイル名を指定"),sg.InputText(key="file_name",default_text="img_",size=(40,1))],
     [sg.Text("カメラ設定変更"),sg.Button("設定変更",key="change")],
+    [sg.Text("解像度変更"),sg.Text("【高さ】"),sg.InputText(default_text=f"{int(default_FRAME_HEIGHT)}",size=(8,1),key="HEIGHT"),sg.Text("×"),sg.Text("【幅】"),sg.InputText(default_text=f"{int(default_FRAME_WIDTH)}",size=(8,1),key="WIDTH"),sg.Button("解像度変更",key="resize")],
     [sg.Image("",key="IMG")],
     [sg.Button(button_text="画像撮影",key="shot",font=("UD デジタル 教科書体 NK-B",30),button_color="red")],
 ]
 window = sg.Window("",layout=lay)
-cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
 
 
 count = 1
 while True:
     event,value = window.read(timeout=0) 
     if event == None:
-        break  
+        break
+    
     ret, frame = cap.read()
     #frameをエンコードしないとpysimpleguiに埋め込めない
     imgbytes = cv2.imencode('.png', frame)[1].tobytes()
@@ -36,7 +43,21 @@ while True:
     if key == ord("q"):
         break
     
+    #カメラ設定変更
     if event == "change":
         cap.set(cv2.CAP_PROP_SETTINGS,0)
     
-    
+    #解像度変更
+    if event == "resize":
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,int(value["HEIGHT"]))
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH,int(value["WIDTH"]))
+        
+        default_FRAME_HEIGHT = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        default_FRAME_WIDTH = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+        window["HEIGHT"].update(int(default_FRAME_HEIGHT))
+        window["WIDTH"].update(int(default_FRAME_WIDTH))
+        #print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+cap.release()#この記述が無いとリサイズ後プログラムが終わらない
+cv2.destroyAllWindows()
--- YoLo/onotes.py
+++ YoLo/onotes.py
@@ -1,34 +1,62 @@
 import datetime
-from time import time
+import PySimpleGUI as sg
 import cv2
-import time
 
+#参考サイト   https://teratail.com/questions/225414
+#            https://pystyle.info/opencv-videoio/
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
 
-camera = cv2.VideoCapture(0)
+fps = int(cap.get(cv2.CAP_PROP_FPS))                    # カメラのFPSを取得
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))              # カメラの横幅を取得
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') # 動画保存時のfourcc設定（mp4用）
+video = cv2.VideoWriter('video_{0}.mp4'.format(1), fourcc, fps, (w, h))
+
+lay = [
+    [sg.Image("",key="IMAGE")],
+    [sg.Button("OK",key="OK")],
+]
+
+window = sg.Window("",lay)
 
 def main(name,frame):
 # 動画ファイル保存用の設定
-    fps = int(camera.get(cv2.CAP_PROP_FPS))                    # カメラのFPSを取得
-    w = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))              # カメラの横幅を取得
-    h = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))             # カメラの縦幅を取得
+    fps = int(cap.get(cv2.CAP_PROP_FPS))                    # カメラのFPSを取得
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))              # カメラの横幅を取得
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))             # カメラの縦幅を取得
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')        # 動画保存時のfourcc設定（mp4用）
     video = cv2.VideoWriter(r'C:\Users\60837\Desktop\syasin\video_{0}.mp4'.format(name), fourcc, fps, (w, h))  # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
     now = datetime.datetime.now()
     TEN=datetime.timedelta(minutes=1)
     while True:
         TT = datetime.datetime.now()
-        ret, frame = camera.read()
-        cv2.imshow("camera", frame)
+        ret, frame = cap.read()
+        cv2.imshow("cap", frame)
         if TT > now + TEN:
             video.write(frame)
             break
  
 count=0
 while True:
-    ret, frame = camera.read()
-    cv2.imshow("camera", frame)
+    event,value = window.read(timeout=0)
+    if event == None:
+        break
     
-    main(count,frame)
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+    
+    imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+    window["IMAGE"].update(imgbytes)
+    
+    video.write(frame)
+    
+    #cv2.imshow("cap", frame)
+    
+    #main(count,frame)
     count +=1
     #video.write(frame)
 
@@ -36,5 +64,5 @@ while True:
         break
     
 # 撮影用オブジェクトとウィンドウの解放
-camera.release()
+cap.release()
 cv2.destroyAllWindows()
